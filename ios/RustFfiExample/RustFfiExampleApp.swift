c2cdb9997ec3a60c013c79158049b004b770c678
blackbracken <me@bracken.black>
2025-07-09 02:36:49 +0900

    feat: initialize ios project

---
+++ ios/RustFfiExample/RustFfiExampleApp.swift
@@ -0,0 +1,12 @@
+//
+
+import SwiftUI
+
+@main
+struct RustFfiExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/RustFfiExampleTests/RustFfiExampleTests.swift
@@ -0,0 +1,12 @@
+//
+
+import Testing
+@testable import RustFfiExample
+
+struct RustFfiExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
